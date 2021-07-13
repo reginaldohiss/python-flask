b0464488727a5253042be1babbb1062c5f9d2778
Sallys Carlos <sallys.carlos93@gmail.com>
2021-07-13 14:18:35 -0300

    Add repository remote

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+TEMPLATES = './templates'
+STATIC = './static'
+
+app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
+
+@app.route('/')
+def helloWorld() :
+  return 'Hello world!'
+
+@app.route('/index')
+def index() :
+  nome = 'Sallys'
+  lista = ['https://www.youtube.com/embed/jJ7p8ObqbHk', 'https://www.youtube.com/embed/vJ-3mQLKxSU', 'https://www.youtube.com/embed/0LB3FSfjvao', 'https://www.youtube.com/embed/N1hTsbW50eM']
+  return render_template('index.html', nome=nome, lista=lista)
+
+app.run(host='0.0.0.0', port=5000)
